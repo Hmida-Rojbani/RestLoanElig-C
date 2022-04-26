d63f4b3e219f5139493e1eaf7ce4124dd44f76fa
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-26 10:57:52 +0100

    Update LoanCtrl.java

--- src/main/java/de/tekup/rest/ctrls/LoanCtrl.java
+++ src/main/java/de/tekup/rest/ctrls/LoanCtrl.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import de.tekup.rest.models.CustomerRequest;
 import de.tekup.rest.models.WsResponse;
 import de.tekup.rest.services.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -16,6 +19,11 @@ public class LoanCtrl {
 	
 	private LoanService service;
 	
+	@Operation(summary = "To check the eligibilty of client")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Return Client status"),
+			@ApiResponse(responseCode = "400", description = "Not yet Implimented")
+	})
 	@PostMapping(path="/check/client/status",
 			consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},
 			produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
